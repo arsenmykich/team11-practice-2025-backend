9986625904568c7ecf4221b50e47e2723527d4c0
arsenmykich <arsenmykich67@gmail.com>
2025-01-23 00:25:17 +0200

    Added Entities and DbContext

---
+++ team11-backend/Data_Access_Layer/Data/CinemaContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Data_Access_Layer.Entities;
+
+namespace Data_Access_Layer.Data
+{
+    internal class CinemaContext : DbContext
+    {
+        public CinemaContext(DbContextOptions<CinemaContext> options) : base(options)
+        {   }
+        public DbSet<Entities.Actor> Actors { get; set; }
+        public DbSet<Entities.Booking> Bookings { get; set; }
+        public DbSet<Entities.Director> Directors { get; set; }
+        public DbSet<Entities.Genre> Genres { get; set; }
+        public DbSet<Entities.Movie> Movies { get; set; }
+        public DbSet<Entities.Role> Roles { get; set; }
+        public DbSet<Entities.SalesStatistics> SalesStatistics { get; set; }
+        public DbSet<Entities.Session> Sessions { get; set; }
+        public DbSet<Entities.User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>()
+                .HasMany(m => m.Actors)
+                .WithMany(a => a.Movies)
+                .UsingEntity<MovieActor>(
+                    am => am.HasOne<Actor>().WithMany(),
+                    am => am.HasOne<Movie>().WithMany(),
+                    am =>
+                    {
+                        am.HasKey(am => new { am.ActorId, am.MovieId });
+                        am.ToTable("MovieActors");
+                    });
+            modelBuilder.Entity<Movie>()
+                .HasMany(m => m.Genres)
+                .WithMany(g => g.Movies)
+                .UsingEntity<MovieGenre>(
+                    mg => mg.HasOne<Genre>().WithMany(),
+                    mg => mg.HasOne<Movie>().WithMany(),
+                    mg =>
+                    {
+                        mg.HasKey(mg => new { mg.GenreId, mg.MovieId });
+                        mg.ToTable("MovieGenres");
+                    });
+            modelBuilder.Entity<Movie>()
+                .HasOne(m => m.Director)
+                .WithMany(d => d.Movies)
+                .HasForeignKey(m => m.DirectorId);
+
+            modelBuilder.Entity<Session>()
+                .HasOne(s => s.Movie)
+                .WithMany(m => m.Sessions)
+                .HasForeignKey(s => s.MovieId);
+            modelBuilder.Entity<SalesStatistics>()
+                .HasOne(ss => ss.Session)
+                .WithMany(s => s.SalesStatistics)
+                .HasForeignKey(ss => ss.SessionId);
+            modelBuilder.Entity<Booking>()
+                .HasOne(s => s.Session)
+                .WithMany(b => b.Bookings)
+                .HasForeignKey(s => s.SessionId);
+            modelBuilder.Entity<Booking>()
+                .HasOne(u => u.User)
+                .WithMany(b => b.Bookings)
+                .HasForeignKey(u => u.UserId);
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId);
+        }
+
+    }
+}
---
+++ team11-backend/Data_Access_Layer/Entities/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Entities
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public string FilmName { get; set; }
+        public Director Director { get; set; }
+        public int DirectorId { get; set; }
+        public string Description { get; set; }
+        public string Trailer { get; set; }
+        public ICollection<Session> Sessions { get; set; }
+        public int Duration { get; set; }
+        public int AgeRating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string PosterPath { get; set; }
+        public ICollection<Actor> Actors { get; set; }
+        public string BackgroundImagePath { get; set; }
+        public float VoteAverage { get; set; }
+        public int VoteCount { get; set; }
+        public ICollection<Genre> Genres { get; set; }
+
+    }
+}
